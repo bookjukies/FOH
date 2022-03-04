f177471593c1ef713810530a5702ea0ccdfdba19
Bongani <mkhumbie34@gmail.com>
2022-03-04 15:38:17 +0200

    production ready music page

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ class HeaderComponent extends HTMLElement {
         <div class="logo"><img src="./images/see.png" alt="FOH RSA"></div>
         <nav>
             <ul>
+                <li><a href="./index.html">Home</a></li>
                 <li><a href="./music.html"">Music</a></li>
-                            <li><a href=" http://www.geniusmuzik.com/search/label/FOH">Bookings</a></li>
-                <li><a href="http://www.geniusmuzik.com/search/label/FOH">Merch</a></li>
+                <li><a href=" http://www.geniusmuzik.com/search/label/FOH">Bookings</a></li>
             </ul>
         </nav>
     </div>
